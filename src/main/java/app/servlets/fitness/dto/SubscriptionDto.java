11b33a056b76650647dc4906f21013e7b67b55c3
ddddddasha <daria.zhelobkovich@gmail.com>
2023-07-17 13:33:38 +0300

    Add dto

--- src/main/java/app/servlets/fitness/dto/SubscriptionDto.java
+++ src/main/java/app/servlets/fitness/dto/SubscriptionDto.java
@@ -1,16 +1,17 @@
 package app.servlets.fitness.dto;
 
+import app.servlets.fitness.entities.enums.SubscriptionCategory;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class SubscriptionDto {
-    private String subscriptionCategory;
+    private SubscriptionCategory subscriptionCategory;
     private String subscriptionName;
     private int subscriptionPrice;
-    private int subscriptionPeriod;
-    private int numberOfGuestVisits;
-    private int maxSubscriptionStop;
+    private int subscriptionDaysNumber;
+    private int numberGuestVisitDays;
+    private int numberSubscriptionStopDays;
     private String description;
 }
--- src/main/java/app/servlets/fitness/dto/UserDto.java
+++ src/main/java/app/servlets/fitness/dto/UserDto.java
@@ -3,12 +3,14 @@ package app.servlets.fitness.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class UserDto {
     private String firstName;
     private String lastName;
-    private int age;
+    private LocalDate dateBirthday;
     private String login;
     private String password;
 }
